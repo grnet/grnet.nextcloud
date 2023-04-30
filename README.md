85f791a3b8aee2e9d79b1bd804387295e57dc18c
Antonis Christofides <achristofides@admin.grnet.gr>
2023-04-30 14:31:03 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# grnet.nextcloud
+
+Ansible collection with roles for installing Nextcloud on Debian/Ubuntu.
+
+Documented at https://grnetnextcloud.readthedocs.com/
---
+++ docs/conf.py
@@ -0,0 +1,9 @@
+project = 'ansible-grnet-nextcloud'
+copyright = '2022-2023 GRNET'
+author = 'Antonis Christofides'
+extensions = [
+]
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+html_theme = 'alabaster'
+html_static_path = ['_static']
